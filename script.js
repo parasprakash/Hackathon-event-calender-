43dcb3be6489bd7ac8777059660e397a205dfbe7
Paras Prakash <parasprakash98@gmail.com>
2021-12-18 15:42:06 +0530

    some more

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 const date = new Date();
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function openModal(date) {
+  clicked = date;
+
+  const eventForDay = events.find(e => e.date === clicked);
+
+  if (eventForDay) {
+    document.getElementById('eventText').innerText = eventForDay.title;
+    deleteEventModal.style.display = 'block';
+  } else {
+    newEventModal.style.display = 'block';
+  }
+
+  backDrop.style.display = 'block';
+}
+
 
 const renderCalendar = () => {
   date.setDate(1);
@@ -59,18 +83,21 @@ const renderCalendar = () => {
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
-      days += `<div class="today">${i}</div>`;
+      days += `<div class="today" onclick="openModal(dd,mm,yy)">${i}</div>`;
     } else {
       days += `<div>${i}</div>`;
     }
+
   }
+  // document.querySelector(".days div").addEventListener("click",()=>openModal(`${date.getMonth()+1}/${i}/${date.getFullYear()}`));
 
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="next-date">${j}</div>`;
-    
   }
+
   monthDays.innerHTML = days;
 };
+
 
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
